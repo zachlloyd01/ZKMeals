89a86059f13b790cfeb84156b6382393f27beb39
zachlloyd01 <zachlloyd01@gmail.com>
2020-06-06 17:27:59 -0400

    Upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
   skip_before_action :authorized, only: [:new, :create]
   def new
     @user = User.new
+    @user.role = "user"
   end
 
   def create
     @user = User.create(params.require(:user).permit(:email, :password, :name, :school, :address))
+    @user.role = "user"
    session[:user_id] = @user.id
    redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_185107) do
+ActiveRecord::Schema.define(version: 2020_06_04_204556) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_185107) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
   end
 
 end
